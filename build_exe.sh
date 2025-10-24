1cc7b044d4a34808c8b36c7083ff9467b5e84504
Nicholas <nsdonaldwebmail@gmail.com>
2025-10-24 14:27:18 -0400

    added test files and folders for automating the CI/CD./CD of the Golang Lambda code (part two of the commit progress from previous commit.).

--- build_exe.sh
+++ build_exe.sh
@@ -1,7 +1,7 @@
 #STEP #1: sh build_exe.sh
 #git add .
-git rm -f bootstrap
-git rm -f "go_lambda_test.zip"
+#git rm -f bootstrap
+#git rm -f "go_lambda_test.zip"
 GOOS=linux CGO_ENABLED=0 GOARCH=arm64 go build -tags lambda.norpc -o bootstrap main.go
 git archive --format=zip --output="go_lambda_test.zip"
 git mv bootstrap go_lambda_test.zip
