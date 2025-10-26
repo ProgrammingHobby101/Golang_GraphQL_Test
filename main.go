397ba98945b5093c0492cea83a424611264d4b9a
Nicholas <nsdonaldwebmail@gmail.com>
2025-10-26 15:01:46 -0400

    updated my instructions in README for roles in AWS CLI.

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 }
 
 func handleHealth() events.LambdaFunctionURLResponse {
-	message := "Hello Healthy World!!!"
+	message := "Hello Healthy World!!!!!"
 	return events.LambdaFunctionURLResponse{StatusCode: 200, Body: message}
 }
 
